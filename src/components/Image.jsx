d03f1d0b58facfeeac3477ef553ddf3ddebcd113
gracemugeche <gracemugeche@gmail.com>
2025-05-04 21:33:11 +0300

    Add image component to display product details

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import product from '../product'; 
 
 function Image() {
-    return <img src="{product.image}" alt="{product.name}"  />
+    return <img src="/serum.jpg" alt="{serum image
+    }"  />
   }
   
   export default Image ;
