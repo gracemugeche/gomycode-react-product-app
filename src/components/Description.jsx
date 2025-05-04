789726ffacdb3a5e1ef075cc4f72ba4aa5678539
gracemugeche <gracemugeche@gmail.com>
2025-05-04 21:29:13 +0300

    Add Description component to display product details

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 //import the JSON object
-import product from '../product'; 
+import product from "../product";
 
 function Description() {
-    return <p>{product.description}</p>; 
-  }
-  
-  export default Description;
+  return (
+    <p>{`Brightens skin tone, reduces dark spots, and gives your face a
+            fresh, radiant glow.`}</p>
+  );
+}
+
+export default Description;
