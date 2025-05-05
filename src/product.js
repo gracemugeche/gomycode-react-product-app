8656917312d6be4517512843f4b9d4c6c6eec8ce
gracemugeche <gracemugeche@gmail.com>
2025-05-05 12:12:16 +0300

    add product details

--- src/product.js
+++ src/product.js
@@ -3,7 +3,7 @@ const product = {
     name: "GlowSkin Vitamin C Serum",
     price: "$24.99",
     description: "Brightens skin tone, reduces dark spots, and gives your face a fresh, radiant glow.",
-    image: "https://via.placeholder.com/150/FF69B4/FFFFFF?text=Vitamin+C+Serum"
+    image: "serum image"
   };
   
   export default product;
